d84b0045e11c95a34250b628adc41f89bed5b9f4
Marius CA <marius@example.com>
2023-03-02 10:23:52 +0200

    nums

--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@ console.log('main.js file was loaded');
 
 // importuojam named exporta
 import { username, town } from './modules/namedExport.js';
+import { nums } from './modules/numbers.js';
 
 console.log(`this users name is ${username} and he is from ${town}`);
+
+// sukurti faila numbers.js
+// jame deklaruoti skaiciu masyva
+// exportuoti
+// importuoti main.js faile
+// atspausdinti main.js faile
+console.log('nums ===', nums);
+// atspausdinti main.js faile vidurki masyvo
+//
---
+++ js/modules/numbers.js
@@ -0,0 +1,4 @@
+'use strict';
+console.log('numbers.js file was loaded');
+
+export const nums = [1, 2, 3, 4];
