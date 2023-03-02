a4deba1b0670c907018b5ac1e54cc5a69d0916ac
Marius CA <marius@example.com>
2023-03-02 11:58:17 +0200

    listNameAgeEmail

--- js/modules/usersHelperFn.js
+++ js/modules/usersHelperFn.js
@@ -13,3 +13,18 @@ export function getNamesArr(arr) {
   return arr.map((uObj) => uObj.name);
   // return arr.map(({ name }) => name);
 }
+
+export const createElement = (el, className = '', text = '', parent = '') => {
+  const newEl = document.createElement(el);
+  if (className) newEl.className = className;
+  if (text) newEl.innerHTML = text;
+  if (parent) parent.appendChild(newEl);
+  return newEl;
+};
+
+export function listNameAgeEmail(arr, dest) {
+  arr.forEach((aObj) => {
+    const elText = `name: ${aObj.name}, age: ${aObj.age}, email: ${aObj.email}`;
+    createElement('p', '', elText, dest);
+  });
+}
--- js/usersDestruct.js
+++ js/usersDestruct.js
@@ -2,11 +2,16 @@
 console.log('usersDestruct.js file was loaded');
 
 import { users } from './db/users.js';
-import { getNamesArr } from './modules/usersHelperFn.js';
+import { getNamesArr, createElement, listNameAgeEmail } from './modules/usersHelperFn.js';
 
 console.table(users);
+
+// taikomes
+const divApp = document.getElementById('app');
 
 const namesArr = getNamesArr(users);
 console.log('namesArr ===', namesArr);
 
-console.log(Object.values(users[0]));
+// createElement('h2', 'card', 'Testing', divApp);
+
+listNameAgeEmail(users, divApp);
